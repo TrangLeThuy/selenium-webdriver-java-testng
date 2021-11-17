99fb60b6945cc88795c0029ea0d895da9019dff1
TrangLT <TrangLT@TrangLT-LA65>
2021-11-17 22:01:34 +0700

    Init repository

---
+++ selenium/Tester/Topic01_System_Progeties.java
@@ -0,0 +1,13 @@
+package Tester;
+
+public class Topic01_System_Progeties {
+	
+
+	public static void main(String[] args) {
+		String projectPath = System.getProperty("user.dir");
+		
+		System.out.println(projectPath);
+	
+	}
+
+}
